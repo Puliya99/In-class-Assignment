56501701d3fc1f73c55cca3edaf1fe9c2fc8ec2f
lmarcho <lakshitha.mathngadeera@neosolax.com>
2024-02-03 11:37:08 +0530

    Commit api

--- src/main/java/lk/ijse/gdse65/saman_api/api/CustomApi.java
+++ src/main/java/lk/ijse/gdse65/saman_api/api/CustomApi.java
@@ -25,6 +25,6 @@ public class CustomApi extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Custom Do Get");
+        System.out.println(req.getRequestURI());
     }
 }
